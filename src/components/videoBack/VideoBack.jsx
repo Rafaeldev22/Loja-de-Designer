82e95e948cb05b3023baa31f243d44ee74401f87
Rafaeldev22 <Rafael.dev.22@gmail.com>
2024-08-12 15:31:56 -0300

    estrutura pagina photoshop montada

---
+++ src/components/videoBack/VideoBack.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import "./videoBack.css";
+
+function VideoBackground() {
+  return (
+    
+  );
+}
+
+export default VideoBackground;
--- src/pages/Photoshop/Photoshop.jsx
+++ src/pages/Photoshop/Photoshop.jsx
@@ -1,16 +1,31 @@
 import Nav from "../../components/navBar/Nav";
-import "./photoshop.css"
+import "./photoshop.css";
+
+// import VideoBackground from "../../components/videoBack/VideoBack";
 
 export default function Photoshop() {
   return (
-    <main>
-      <header>
-        <Nav />
-      </header>
+    <>
+      <div className="video-container">
+        <video autoPlay loop muted playsInline className="video-background">
+          <source src="https://i.imgur.com/hkzEp0v.mp4" type="video/mp4" />
+          Seu navegador não suporta vídeos em HTML5.
+        </video>
+      </div>
 
-        <div>
-            <h1>Página photoshop</h1>
-        </div>
-    </main>
+      <main>
+        <header>
+          <Nav />
+        </header>
+
+        <section className="sessao-1">
+          <div className="box">
+            <div className="boxinha">
+              <h1>Página photoshop</h1>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
